1469dd0ee8c365ccbd4287482824f7ead8082759
Dblackone <61014349+Dblackone@users.noreply.github.com>
2020-03-26 12:19:48 +0100

    update

---
+++ javascrip task/index.js
@@ -0,0 +1,17 @@
+let name = "Jeff";
+
+ console.log("My name is: " + name);
+
+ const data = [];
+
+ for (let i=0; i<=100; i++) {
+
+   if (i%2===0) {
+
+     data.push(i);
+
+   }
+
+ }
+
+ console.log(data);
