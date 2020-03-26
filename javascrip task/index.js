daaa6d40991c719cb20e481c5a084075fb29187f
Dblackone <61014349+Dblackone@users.noreply.github.com>
2020-03-26 17:44:41 +0100

    update

--- javascrip task/index.js
+++ javascrip task/index.js
@@ -1,17 +1,3 @@
-let name = "Jeff";
-
- console.log("My name is: " + name);
-
- const data = [];
-
- for (let i=0; i<=100; i++) {
-
-   if (i%2===0) {
-
-     data.push(i);
-
-   }
-
- }
-
- console.log(data);
+var myName = "Akarakiri vollmann";
+document.getElementById("demo").HTML =
+"My name is" + myName;
